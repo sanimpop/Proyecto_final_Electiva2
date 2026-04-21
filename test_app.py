3711105981ae472e513f34634c5a61b761b68bfa
Izmael Planco <Izmaelofficial@gmail.com>
2026-04-21 12:13:25 -0400

    Fix test correcto

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hola Mundo, aqui ismael polanco (Matricula 2021-0293(!)" in response.data
+    assert b"Hola Mundo, aqui ismael polanco (Matricula 2021-0293)!" in response.data
