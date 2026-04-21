b7142f51dba3c5324a4edda41e33e42382e1ec78
Izmael Planco <Izmaelofficial@gmail.com>
2026-04-21 12:06:51 -0400

    arreglando pytest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola Mundo, aqui ismael polanco (Matricula 2021-0293(!"
+    return "Hola Mundo, aqui ismael polanco (Matricula 2021-0293)!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
